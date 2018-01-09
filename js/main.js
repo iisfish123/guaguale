17dd42f8355c8a3af3fb044a0753401cf0c57edd
645568057@qq.com <15920328477>
2018-01-10 02:09:43 +0800

    随便

--- js/main.js
+++ js/main.js
@@ -58,6 +58,6 @@ $(function () {
         }
     }
     window.game = game;
-    window.app = app;
+    window.app = app
     game.init()//如果失败，应该是图片加载问题,解决方案最好是异步加载完图片，再执行game.init。  或者直接打开demo，在控制台输入game.init()也能看到效果
 })
